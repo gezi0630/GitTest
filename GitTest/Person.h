b6bcdf2cbab31dceb81ccfb49760376c731f9f71
gezi0630 <907768934@qq.com>
2018-03-15 14:39:41 +0800

    新增Person类

---
+++ GitTest/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GitTest
+//
+//  Created by MAC on 2018/3/15.
+//  Copyright © 2018年 GuoDongge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ GitTest/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  GitTest
+//
+//  Created by MAC on 2018/3/15.
+//  Copyright © 2018年 GuoDongge. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
