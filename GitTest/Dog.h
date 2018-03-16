0ccf7ca0fe9d6638817844e750c0041f283ad6da
gezi0630 <907768934@qq.com>
2018-03-16 16:49:44 +0800

    新增dog类

---
+++ GitTest/Dog.h
@@ -0,0 +1,13 @@
+//
+//  Dog.h
+//  GitTest
+//
+//  Created by MAC on 2018/3/16.
+//  Copyright © 2018年 GuoDongge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dog : NSObject
+
+@end
---
+++ GitTest/Dog.m
@@ -0,0 +1,13 @@
+//
+//  Dog.m
+//  GitTest
+//
+//  Created by MAC on 2018/3/16.
+//  Copyright © 2018年 GuoDongge. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
